2ac16e8b3b1a408a7fa345f4b2a1baa13de64faa
Sugeyri <sugeyrirojas@gmail.com>
2017-08-13 01:25:23 -0600

    muestra clientes en tabla

--- PROYECTO-DIDANO/js/agregar.js
+++ PROYECTO-DIDANO/js/agregar.js
@@ -1,4 +1,4 @@
-
+var listaCliente =[];
 
 $(document).ready(function() {
     $('#agregar').click(function () {
@@ -6,7 +6,11 @@ $(document).ready(function() {
 ruta: document.getElementById("ruta").value, responsable: document.getElementById("responsable").value, 
 direccion: document.getElementById("direccion").value, telefono: document.getElementById("telefono").value};
 
+console.log(nuevoCliente);
+listaCliente.push(nuevoCliente);
+
 localStorage.setItem('datos-cliente', JSON.stringify(nuevoCliente));
+mostrarCliente();
 
  document.getElementById("codigo").value =""; 
  document.getElementById("nombre").value ="";
@@ -17,5 +21,36 @@ localStorage.setItem('datos-cliente', JSON.stringify(nuevoCliente));
 
     });
 });
-	
+
+function mostrarCliente(){
+	 var lista = listaCliente,
+	  tbody = document.querySelector('#cliente-tabla tbody');
+
+	  
+
+	  for(var i =0; i<lista.length; i++){
+		  var row = tbody.insertRow(i);
+			 codCell = row.insertCell(0),
+			 nomCell =row.insertCell(1),
+			 rutaCell = row.insertCell(2),
+			 respCell = row.insertCell(3),
+			 dirCell = row.insertCell(4),
+			 telCell = row.insertCell(5),
+			 selectCell = row.insertCell(6);
+
+			codCell.innerHTML = lista[i].codigo;
+			nomCell.innerHTML = lista[i].nombre;
+			rutaCell.innerHTML = lista[i].ruta;
+			respCell.innerHTML = lista[i].responsable;
+			dirCell.innerHTML = lista[i].direccion;
+			telCell.innerHTML = lista[i].telefono;
+
+			var inputSelect = document.createElement('input');
+			inputSelect.type = 'radio';
+			inputSelect.value = lista[i].codigo;
+			selectCell.appendChild(inputSelect);
+
+			tbody.appendChild(row);
+	  }
+}
 	

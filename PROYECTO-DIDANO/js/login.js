ec5d8b7867ff15524de5237fa447a4fe57f1f66a
Sugeyri <sugeyrirojas@gmail.com>
2017-08-18 01:56:56 -0600

    login

--- PROYECTO-DIDANO/js/login.js
+++ PROYECTO-DIDANO/js/login.js
@@ -1,75 +1,45 @@
-function extraerDatos(){
-    //se extraen los datos de las cajas de texto
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var usuario = document.getElementById("usuario").value;
-    var email = document.getElementById("email").value;
-    var clave = document.getElementById("clave").value;
-    var nclave = document.getElementById("nclave").value;
-    
-    var datos = {nombre: nombre, apellido: apellido, usuario: usuario, fecha: fecha, email:email, clave:clave, nclave:nclave};
-    //la variable "datos" almacena toda la información del usuario en formato Json
-    return datos;
+function annadirEventos() {
+	var bIngresar = document.getElementById("ingresar");
+	bIngresar.addEventListener("click", verificarDatos);
+	var bInvitado = document.getElementById("invitado");
+	bInvitado.addEventListener("click", invitado);
+}
+annadirEventos();
+
+function invitado(){
+	location.href = "inicio.html";
 }
 
+function verificarDatos() {
+	var nUsuario = document.getElementById("user").value;
+	var pass = document.getElementById("pass").value;
 
+	if (nUsuario == "" || pass == "") {
+		alert("Lo sentimos! Hay campos vacios!");
+	} else {
+		var users = JSON.parse(localStorage.getItem('usuarios'));
+		var element;
 
-function validarDatos(){
-    debugger;
-    
-    var user = extraerDatos();
-    var estado = false;
-    var users = JSON.parse(localStorage.getItem("users"));
-    
-    //condicional verifica que las contraseñas que el usuario ingreso en la pantalla de registro sean las mismas
-    if(user.clave == user.nclave){
-        //condicional verifica si localstorage esta vacio 
-        if(!users){
-            users = [];//crea arreglo
-            users.push(user);//le inserta el nuevo usuario 
-            localStorage.setItem("users", JSON.stringify(users));//lo almacena en localstorage
-            alert("Registro exitoso!");
-            
-        }else{
-            for(var i in users)
-            {   //se compara con todos los usuarios de localstore y si existe 
-                //esta variable boolean cambia de estado a "true" y envia un mensaje 
-                    estado = true;
-                if(users[i].usuario == user.usuario){
-                    alert("El usuario ya existe!");
-                }
-            
-            }
-                //se compara la variable estado, si esta "falso" quiere decir que el nombre de usuario
-                //se encuentra disponible y el registro se realizara exitosamente
-                if(estado == false){ 
-                    users.push(user);
-                    localStorage.setItem("users", JSON.stringify(users));
-                    alert("Registro exitoso!");
-                }
-        
-        }
-        
-    }else{
-        alert("¡Claves diferentes!")
-        setTimeout (redireccionar(), 5000);
-    }
-    
-        
-    }
-
-
-function Agregar(){
-    var obj=document.getElementById("guardar");
-    obj.addEventListener('click', function(){//evento del boton "click"
-        validarDatos();  //llama a la funcion "ValidarDatos"  
-    });
+		for (var i = 0; i < users.length; i++) {
+			element = users[i];
+			if (nUsuario == element.usuario) {
+				if (pass == element.pass) {
+					localStorage.setItem("usuarioLogeado", nUsuario);
+					location.href = "menu-principal.html";
+					break;
+				} else {
+					alert("Lo sentimos! Contraseña incorrecta!")
+					break;
+				}
+			} else if (i == users.length - 1) {
+				limpiarCampos();
+				alert("Lo sentimos! " + nUsuario + " no se encuentra registrado.")
+			}
+		}
+	}
 }
 
-Agregar();
-
-
-function redireccionar(){
-  window.locationf="file:///C:/Users/Toshiba/Documents/UTN/V%20CUATRI/APLICACION%20WEB/PROYECTOS-DISE-O-DE-APLIACIONES-WEB/PROYECTO-DIDANO/iniciar-sesion.html";
-} 
-
+function limpiarCampos() {
+	document.getElementById("user").value = "";
+	document.getElementById("pass").value = "";
+}
